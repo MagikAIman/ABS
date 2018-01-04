c4d77902ce1c7832be24f3967d149f7883ad0686
Kelvin <alaricbaum94@gmail.com>
2018-01-04 08:12:35 -0800

    Added Error redirecting file for chapter three'

---
+++ AdvBash3/redirectError.sh
@@ -0,0 +1,8 @@
+command_test () { type "$1" &>/dev/null; }
+#
+
+cmd=rmdir	# Legitimate command.
+command_test $cmd; echo $?   # 0
+
+cmd=bogus_command    # Illegitimate command
+command_test $cmd; echo $? # 1
